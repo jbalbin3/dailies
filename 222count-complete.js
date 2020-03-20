7c92cd61af9caa17e9e4be26762833aa94497f27
John Balbin <jbalbin3@yahoo.com>
2020-03-19 18:42:19 -0700

    filled in function

---
+++ 222count-complete.js
@@ -0,0 +1,42 @@
+/*
+Given a complete binary tree, count the number of nodes.
+
+Note:
+
+Definition of a complete binary tree from Wikipedia:
+In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Example:
+
+Input:
+    1
+   / \
+  2   3
+ / \  /
+4  5 6
+
+Output: 6
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+var countNodes = function(root) {
+  var count = 0;
+  if(!root) { return count; }
+  if(root.val) {
+    count += 1;
+  }
+  if(root.left) {
+    count += countNodes(root.left);
+  }
+  if(root.right) {
+    count += countNodes(root.left);
+  }
+  return count;
+};
