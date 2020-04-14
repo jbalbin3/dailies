81340b6d9cfde0050e733ef955f954375a2d9e9e
John Balbin <jbalbin3@yahoo.com>
2020-04-14 15:10:02 -0700

    updated function

--- findMaxLength.js
+++ findMaxLength.js
@@ -1,6 +1,8 @@
+
 var findMaxLength = function(nums) {
   var max = count = 0;
   if(nums.length < 2) { return max; }
+
   var index = {0: -1};
   for(let i in nums) {
     count += nums[i] ? 1 : -1;
@@ -10,5 +12,48 @@ var findMaxLength = function(nums) {
       index[count] = i;
     }
   }
+  console.log(max);
   return max;
 };
+
+findMaxLength([1,1,1,1]) // 0
+findMaxLength([1,1,1]) // 0
+findMaxLength([0,0,0,0]) // 0
+findMaxLength([0,1]) // 2
+findMaxLength([0,1,0]) // 2
+findMaxLength([1,1,0]) // 2 <---
+findMaxLength([0,0,0,1,0,0]) // 2 <---
+findMaxLength([0,1,0,1]) // 4
+findMaxLength([0,1,0,1,0]) // 4
+findMaxLength([1,0,0,1]) // 4
+findMaxLength([1,1,0,0,1,0]) // 6
+findMaxLength([0,0,0,1,1,1]) // 6
+findMaxLength([0,0,0,1,0,1]) // 4 <---
+
+// findMaxLength([1,0,0,0,1,1,1]) // 6
+// findMaxLength([0,0,0,1,1,1,0,1]) // 8
+// findMaxLength([0,0,0,1,1,1,0,1]) // 8
+
+// findMaxLength([0,1,1,0,1,0]) // 6
+
+// findMaxLength([0,1,1,0,1,1,0]) // 4
+
+// findMaxLength([0,1,1,0,1,1,1,0]) // 4
+// findMaxLength([0,0,1,0,0,0,1,1,0]) // 6 <--
+// findMaxLength([0,0,1,0,0,0,1,1,1]) // 8 <--
+
+// findMaxLength([0,0,1,0]) // 2 <--
+
+findMaxLength(
+  [1,1,1,1,1,1,1,0,0,0,
+   0,1,1,0,1,0,0,1,1,1,
+   1,1,1,1,1,1,0,0,0,0,
+   1,0,0,0,0,1,0,1,0,0,
+   0,1,1,0,0,0,0,1,0,0,
+   1,1,1,1,1,0,0,1,0,1,
+   1,0,0,0,1,0,0,0    ,1,1,
+   1,0,1,1,0,1,0,0,1,1,
+   0,1,0,0,1,1,1,0,0,1,
+   0,                  1,1,1,0,0,1,0,1,1]); // 94
+
+findMaxLength([0,1,0,1,1,1,0,0,1,1,0,1,1,1,1,1,1,0,1,1,0,1,1,0,0,0,1,0,1,0,0,1,0,1,1,1,1,1,1,0,0,0,0,1,0,0,0,1,1,1,0,1,0,0,1,1,1,1,1,0,0,1,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,1,0,1,0,1,1,0,0,1,1,0,1,1,1,1,0,1,1,0,0,0,1,1]);
